4405c3ce4181c648ab5f849141ce284c13bfde57
Milla Lintunen <milla.lintunen@hotmail.com>
2019-07-31 15:01:15 +0300

    näkymästä toiseen siirtymisen lisääminen ylävalikkoon

--- application/quotes/views.py
+++ application/quotes/views.py
@@ -19,3 +19,23 @@ def quotes_create():
     db.session().commit()
   
     return  redirect(url_for("quotes_index"))
+
+# sivun haku kun sanontaa halutaan muokata
+@app.route("/quotes/modifyState/<quote_id>/", methods=["GET"])
+def quotes_modifyState(quote_id):
+
+    q = Quote.query.get(quote_id)
+    
+    return render_template("quotes/modifyState.html", quote_id = q)
+
+@app.route("/quotes/<quote_id>/", methods=["GET", "POST"])
+def quotes_update(quote_id):
+
+    quote = Quote.query.get(quote_id)
+    q = Quote(request.form.get("quote"))
+
+    quote.quote = q
+    
+    db.session().commit()
+
+    return redirect(url_for("quotes_index"))
